e6139691963cd043c02608ecc82de50db6664370
Alex Agosto <alex.agosto@upr.edu>
2021-11-29 14:57:06 -0400

    Made own simple lexer, will add more operands and tokens once I verify functionality.

--- Lexer.py
+++ Lexer.py
@@ -1,65 +1,145 @@
 #imports for lexer
 import sys
+from typing import Text
 sys.path.insert(0, "../..")
-import ply.lex as lex
 
-#General use words for language
-tokens = [
-    'NUMBER',
-    'ID',
-    'LPAREN',
-    'RPAREN'
-]
-#Specific purpose words for language
-reserved = {
-    'plus': 'PLUS',
-    'minus': 'MINUS',
-    'times': 'TIMES',
-    'dividedBy': 'DIVIDEDBY',
-    'display': 'DISPLAY',
-    'for': 'FOR',
-    'if': 'IF',
-    'else': 'ELSE',
-    'while': 'WHILE',
-    'greaterThan': 'GREATERTHAN',
-    'lessThan': 'LESSTHAN',
-    'equals': 'EQUALS',
-    'end': 'END',
-    'and': 'AND',
-    'exact': 'EXACT',
-    'between': 'BETWEEN',
-    'endBlock': 'ENDBLOCK',
-    'att': 'ATTRIBUTE'
-}
-#Added values of reserved words to list.
-tokens += list(reserved.values())
+#TOKENS
+TT_INT = 'TT_INT'
+TT_FLOAT = 'TT_FLOAT'
+TT_PLUS = 'TT_PLUS'
+TT_MINUS = 'TT_MINUS'
+TT_MUL = 'TT_MUL'
+TT_DIV = 'TT_DIV'
+TT_LPAREN = 'TT_LPAREN'
+TT_RPAREN = 'TT_RPAREN'
 
-#Token Specifications
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+#CONSTANTS
+DIGITS = '0123456789'
 
-#Check for reserved words. 
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value,'ID')
-    return t
 
-#CleanUp and extra declarations for ease of use.
-t_ignore = " \t" 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-def t_error(t):
-    print("Illegal Character '%s'" % t.value[0])
-    t.lexer.skip(1)
-reserved_map = { }
-for r in reserved:
-    reserved_map[r.lower()] = r
+#ERRORS
 
-#Construct lexer
-lexer = lex.lex()
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        errorLog = f'{self.error_name}: {self.details}'
+        return errorLog
+
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__('Illegal Character', details)
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+    
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+
+#Lexer 
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+
+            if self.current_char in ' \t':
+                self.advance()
+
+            elif self.current_char in DIGITS:
+                tokens.append(self.makeNum())
+
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+
+            else:
+                char = self.current_char
+                self.advance()
+                return [],IllegalCharError("'" + char + "'")
+
+        return tokens, None
+
+
+    def make_number(self):
+        num_str = ''
+        period_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if period_count == 1: break 
+                period_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+        
+        if period_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT , float(num_str))
+        
+
+
+#RUN LEXER
+def run(text):
+    lexer = Lexer(text)
+    tokens, error  = lexer.make_tokens()
+
+    return tokens, error
+
+
+# reserved = {
+#     'display': 'DISPLAY',
+#     'for': 'FOR',
+#     'if': 'IF',
+#     'else': 'ELSE',
+#     'while': 'WHILE',
+#     'greaterThan': 'GREATERTHAN',
+#     'lessThan': 'LESSTHAN',
+#     'equals': 'EQUALS',
+#     'end': 'END',
+#     'and': 'AND',
+#     'exact': 'EXACT',
+#     'between': 'BETWEEN',
+#     'endBlock': 'ENDBLOCK',
+#     'att': 'ATTRIBUTE'
+# }
+
+
+
 
