a0bc013566f33cd807a89072af89cb585772fe1a
Alex Agosto <alex.agosto@upr.edu>
2021-11-29 01:16:38 -0400

    Fixed lines 7-14 so code runs

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ print('Initializing EasyCode')
 while(True):
     try:
         text = input('EasyCode > ')
-        except EOFError:
-            break
-        except KeyboardInterrupt:
-            break
-        if not text: continue
-        #result = parse and lex here btw
-        if not result == None:
-            print(result)
+    except EOFError:
+        break
+    except KeyboardInterrupt:
+        break
+    if not text: continue
+    #result = parse and lex here btw
+    if not result == None:
+        print(result)
