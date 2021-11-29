c2314f4069786b029caa277fae59685dc42651b9
Alex Agosto <alex.agosto@upr.edu>
2021-11-29 15:50:22 -0400

    same as last commit

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import Parser as parser
 print('Initializing EasyCode')
 while(True):
     text = input('EasyCode > ')
-    result, error = lexer.run(text)
+    result, error = lexer.run('<stdin>', text)
     #result = parser.parser.parse(text)
     if error: print(error.as_string)
     else: print(result)
