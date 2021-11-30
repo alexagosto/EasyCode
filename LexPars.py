994b3eb1d2cd82d7bd9cfa31bdaded388b43ca63
Alex Agosto <alex.agosto@upr.edu>
2021-11-30 18:56:57 -0400

    Updated grammar, symboltable refferences and token list for Functions.

--- LexPars.py
+++ LexPars.py
@@ -3,7 +3,7 @@ import sys
 from typing import Text
 from arrows import *
 import string
-import SymbolTable as symboltable
+from SymbolTable import *
 sys.path.insert(0, "../..")
 
 
@@ -35,6 +35,23 @@ TT_ID = 'ID'
 TT_KEYWORD = 'KEYWORD'
 TT_EQ = 'EQ'
 
+
+
+#4th stage
+TT_EXACT = 'EXACT'
+TT_NE = 'NOTEQUALS'
+TT_LT = 'LESSTHAN'
+TT_GT = 'GREATERTHAN'
+TT_LTE = 'LESSTHANEQUALS'
+TT_GTE = 'GREATERTHANEQUALS'
+
+#5th stage
+TT_COMMA = 'COMMA'
+TT_ARROW = 'ARROW'
+
+END = 'END'
+
+
 #Expand Later on
 VARLIST = [
     'VAR', #3RD
@@ -48,19 +65,11 @@ VARLIST = [
     'FOR', #6th
     'TO',  #6th
     'STEP',#6th
-    'WHILE'#7th
+    'WHILE',#7th
+    'FUN' #8th
 
 ]
 
-#4th stage
-TT_EXACT = 'EXACT'
-TT_NE = 'NOTEQUALS'
-TT_LT = 'LESSTHAN'
-TT_GT = 'GREATERTHAN'
-TT_LTE = 'LESSTHANEQUALS'
-TT_GTE = 'GREATERTHANEQUALS'
-
-END = 'END'
 
 #CONSTANTS
 DIGITS = '0123456789'
@@ -76,53 +85,50 @@ LETTERS_DIGITS = LETTERS + DIGITS
 
 # ERRORS
 class Error:
-    def __init__(self, pos_start, pos_end, error_name, details):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
-        self.error_name = error_name
-        self.details = details
-    
-    def as_string(self):
-        result  = f'{self.error_name}: {self.details}\n'
-        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-        return result
+	def __init__(self, pos_start, pos_end, error_name, details):
+		self.pos_start = pos_start
+		self.pos_end = pos_end
+		self.error_name = error_name
+		self.details = details
+	
+	def as_string(self):
+		result  = f'{self.error_name}: {self.details}\n'
+		result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		return result
 
 class IllegalCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 class ExpectedCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Expected Character', details)
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end, 'Expected Character', details)
 
 class InvalidSyntaxError(Error):
-    def __init__(self, pos_start, pos_end, details=''):
-        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+	def __init__(self, pos_start, pos_end, details=''):
+		super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 class RTError(Error):
-    def __init__(self, pos_start, pos_end, details, context):
-        super().__init__(pos_start, pos_end, 'Runtime Error', details)
-        self.context = context
+	def __init__(self, pos_start, pos_end, details, context):
+		super().__init__(pos_start, pos_end, 'Runtime Error', details)
+		self.context = context
 
-    def as_string(self):
-        result  = self.generate_traceback()
-        result += f'{self.error_name}: {self.details}'
-        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-        return result
+	def as_string(self):
+		result  = self.generate_traceback()
+		result += f'{self.error_name}: {self.details}'
+		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		return result
 
-    def generate_traceback(self):
-        result = ''
-        pos = self.pos_start
-        ctx = self.context
-
-        while ctx:
-            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
-            pos = ctx.parent_entry_pos
-            ctx = ctx.parent
-
-        return 'Traceback (most recent call last):\n' + result
-
+	def generate_traceback(self):
+		result = ''
+		pos = self.pos_start
+		ctx = self.context
+		while ctx:
+			result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+			pos = ctx.parent_entry_pos
+			ctx = ctx.parent
+		return 'Traceback (most recent call last):\n' + result
 
 # POSITION
 class Position:
@@ -952,7 +958,7 @@ class Interpreter:
 ##############################################################################################################
 
 #Global Symbol Table
-global_symbol_table = symboltable.SymbolTable()
+global_symbol_table = SymbolTable()
 global_symbol_table.set("NULL", Number(0))
 global_symbol_table.set("FALSE", Number(0))
 global_symbol_table.set("TRUE", Number(1))
