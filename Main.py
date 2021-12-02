84633932882a1a2660c78080b1df8edfc0108e17
Alex Agosto <alex.agosto@upr.edu>
2021-12-01 20:28:03 -0400

    Comments to main

--- Main.py
+++ Main.py
@@ -1,20 +1,24 @@
-import LexPars
+#Imports for main
 import LexNatLan
+import LexPars
+
+
+#initialization of run function from lexpars
 print('Initializing EasyCode')
 while(True):
+    #Console input + natural language change.
     text = input('EasyCode > ')
-    
     text = LexNatLan.run(text)
 
-    if text.strip() == "":
-        continue
+    #Verifies for single line code
+    if text.strip() == "": continue
     result, error = LexPars.run('<stdin>', text)
-    if error:
-        print(error.as_string)
+
+    if error: print(error.as_string)
+
     elif result:
-        if len(result.elements) == 1:
-            print(repr(result.elements[0]))
-        else:
-            print(repr(result))
+        if len(result.elements) == 1: print(repr(result.elements[0]))
+        else: print(repr(result))
+
 
 # Run this python file and enjoy EasyCode. The following are possible operations and syntax that EasyCode allows you to do!
